3e730d3a6546dbfc248cdc87056bc02b5b30d6c7
Xijaxa <81866800+Xijaxa@users.noreply.github.com>
2023-07-15 13:36:28 +0300

    Main

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import "./styles.css";
 
 document.getElementById("app").innerHTML = `
 <h1>Hello World!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
+<main>
+<button id="my-button">My-button</button>
+</main>
+
+
 `;
+
+const myButton = document.getElementById("my-button");
+
+myButton.addEventListener("click", function() {
+  console.log("hello world")
+});
