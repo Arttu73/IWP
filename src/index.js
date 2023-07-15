9108409c494ef52e768ba2d7b043d1a12d059c32
Xijaxa <81866800+Xijaxa@users.noreply.github.com>
2023-07-15 14:14:00 +0300

    Main

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const addData = document.getElementById("add-data");
 
 myButton.addEventListener("click", function() {
   console.log("hello world")
-  const text = document.getElementsByTagName("h1");
+  const text = document.getElementsByTagName("h1")[0];
   text.innerText = "My notebook";
 });
 
@@ -26,6 +26,6 @@ addData.addEventListener("click", function(){
   const unoList = document.getElementsByTagName("ul");
   const listItem = document.createElement("li")
   /*listItem.innerText = "arbitrary text";*/
-  listItem.innerText = textArea.getElementById("textarea").value;
+  listItem.innerText = document.getElementById("textarea").value;
   unoList.append(listItem);
 } );
